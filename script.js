4e261cf86eccce9224f6a43e686b8b1993a6d9ba
Ahmdaymn101 <132311684+Ahmdaymn101@users.noreply.github.com>
2025-05-08 10:23:55 +0300

    script.js

--- script.js
+++ script.js
@@ -53,18 +53,22 @@ let rsaKeys = {
 // Toggle password visibility
 passwordToggles.forEach(toggle => {
     toggle.addEventListener('click', function () {
-        const inputId = this.dataset.target;
+        const inputId = this.getAttribute('data-target');
         const input = document.getElementById(inputId);
         const icon = this.querySelector('i');
 
-        if (input.type === 'password') {
-            input.type = 'text';
-            icon.classList.remove('fa-eye');
-            icon.classList.add('fa-eye-slash');
+        if (input && icon) {
+            if (input.type === 'password') {
+                input.type = 'text';
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            } else {
+                input.type = 'password';
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            }
         } else {
-            input.type = 'password';
-            icon.classList.remove('fa-eye-slash');
-            icon.classList.add('fa-eye');
+            console.error('Invalid target or icon for password toggle.');
         }
     });
 });
